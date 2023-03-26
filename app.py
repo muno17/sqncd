b8777edfd31d46de2f773a846e1d9f1630267a2b
muno17 <munozd17@gmail.com>
2023-03-26 19:01:17 -0400

    link together pages with flask/jinja

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/sqncd')
+def sqncd():
+    return render_template('sqncd.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
