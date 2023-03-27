59f8a91fd425ab9fe783e7071b696c1977eff42b
muno17 <munozd17@gmail.com>
2023-03-26 22:19:52 -0400

    add alternate color to buttons 5-8 and 13-16

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 //create 4x16 grid for buttons
 let buttonContainer = document.getElementById('buttonContainer')
+let altColor = [5, 6, 7, 8, 13, 14, 15, 16]
 
 function gridCreator() {
     for (let i = 1; i < 5; i++) {
@@ -10,6 +11,11 @@ function gridCreator() {
         for (let j = 1; j < 17; j++) {
             let gridButton = document.createElement('button');
             gridButton.setAttribute('id', 'gridButton');
+
+            if (altColor.includes(j)) {
+                gridButton.style.backgroundColor = "#BC81BF";
+            }
+
             gridRow.appendChild(gridButton);
         console.log('all the way in')
         }
