956b2a881d76e4e0416a93603c2edb2a9013aafa
muno17 <munozd17@gmail.com>
2023-03-28 18:29:32 -0400

    add error page, start linking inputs from forms to app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from werkzeug.wrappers import Request, Response
 
 app = Flask(__name__)
 
@@ -22,23 +23,41 @@ def account():
     return render_template('account.html')
 
 
-@app.route ('/login')
+@app.route ('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        hashedPassword = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8 )
+        return render_template('login.html')
+
     return render_template('login.html')
 
 
-@app.route ('/register')
+@app.route ('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+            username = request.form.get('username')
+            email = request.form.get('email')
+            password = request.form.get('password')
+            cpassword = request.form.get('cpassword')
+
+
+
+            return render_template('register.html')
+
     return render_template('register.html')
  
 
-@app.route ('/passwordChange')
-def passwordChange():
-    return render_template('passwordChange.html')
-
-@app.route ('/passwordReset')
+@app.route ('/passwordReset', methods=["GET", "POST"])
 def passwordReset():
     return render_template('passwordReset.html')
+
+
+@app.route ('/passwordChange', methods=["GET", "POST"])
+def passwordChange():
+    return render_template('passwordChange.html')
 
 
 @app.route ('/news')
@@ -49,6 +68,11 @@ def news():
 @app.route ('/donate')
 def donate():
     return render_template('donate.html')
+
+
+@app.route ('/error')
+def error():
+    return render_template('error.html')
 
 
 
