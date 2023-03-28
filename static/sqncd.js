acbf999dba5e42d083619295fc8ab3e10cdbdeef
muno17 <munozd17@gmail.com>
2023-03-28 09:01:35 -0400

    add WebMidi

--- static/sqncd.js
+++ static/sqncd.js
@@ -1,3 +1,33 @@
+WebMidi.enable();
+console.log(WebMidi.inputs)
+console.log(WebMidi.outputs)
+
+let output = WebMidi.outputs[0];
+let channel = output.channels[1];
+//channel.playNote('C3', {duration: 1000})
+
+
+
+// add functionality to midi dropdown menu
+let midiButton = document.getElementById('midiButton');
+let midiClick = document.getElementById('midiChannelHouse');
+midiButton.addEventListener('click', () => {
+    if (midiClick.style.display === 'none') {
+        midiClick.style.display = 'block';
+    } else {
+        midiClick.style.display = 'none';
+    }
+})
+// COME BACK TO THIS
+
+
+
+
+
+
+
+
+
 //create 4x16 grid for buttons
 let buttonContainer = document.getElementById('buttonContainer')
 let altColor = [5, 6, 7, 8, 13, 14, 15, 16]
@@ -7,7 +37,7 @@ function sqncr() {
         let gridRow = document.createElement('div');
         gridRow.setAttribute('class', 'gridRow');
         buttonContainer.appendChild(gridRow);
-        
+
         for (let j = 1; j < 17; j++) {
             let gridButton = document.createElement('button');
             gridButton.setAttribute('class', 'gridButton');
@@ -44,18 +74,6 @@ function sqncr() {
 // call gridCreator() when the page loads
 window.addEventListener('load', () => sqncr())
 
-
-// add functionality to dropdown menu
-let midiButton = document.getElementById('midiButton');
-let midiClick = document.getElementById('midiChannelHouse');
-midiButton.addEventListener('click', () => {
-    if (midiClick.style.display === 'none') {
-        midiClick.style.display = 'block';
-    } else {
-        midiClick.style.display = 'none';
-    }
-})
-// COME BACK TO THIS
 
  
 
