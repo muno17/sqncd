63c00ce8d035b2da8660a4a2c2d7f337da76dccf
muno17 <munozd17@gmail.com>
2023-04-01 22:17:21 -0400

    move webmidi initialization and io to midi_io

--- static/javascript/midi_io.js
+++ static/javascript/midi_io.js
@@ -1,2 +1,45 @@
-// choose device
-// choose midi channel
+// check if browser supports WebMIDI
+if (navigator.requestMIDIAccess) {
+    console.log('This browser supports WebMIDI');
+} else {
+    console.log('WebMIDI is not supported in this browser');
+}
+
+
+WebMidi.enable().then(sendMidi).catch(err => console.log(''));
+
+let devices = []
+
+// give channel base value, change when new channel is selected
+let channel = 1
+let selectedChannel = document.getElementById('channelDropdown');
+selectedChannel.addEventListener('change', () => {
+    channel = selectedChannel.value;
+})
+
+// Function triggered when WEBMIDI.js is ready
+function sendMidi(n) {
+// Display available MIDI input devices
+    let note = n
+    if (WebMidi.outputs.length < 1) {
+    console.log("No device detected.");
+    // pass this into dropdown as only option, disable midi channels
+    } else {
+    WebMidi.outputs.forEach((device, index) => {
+        //console.log(`${index}: ${device.name}`);
+        // add to devices array to be able to reference in dropdowns
+        devices.push(`${index}: ${device.name}`);
+
+        // pass in device and midi channel to send to
+        let outputDevice = WebMidi.getOutputByName(device['name']);
+        let channel = outputDevice.channels[1];
+        
+        //console.log(device['name'])
+        channel.playNote(n, {duration: 300})
+
+        // add eventListener for notes function
+
+    })
+    }
+
+}
