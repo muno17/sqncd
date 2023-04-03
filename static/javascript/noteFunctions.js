7a1d23d6ee21648f8c7574c270b8f021ae35b96a
muno17 <munozd17@gmail.com>
2023-04-03 19:11:43 -0400

    add eventListeners to noteFunction buttons

---
+++ static/javascript/noteFunctions.js
@@ -0,0 +1,62 @@
+let accent = document.getElementById('accent')
+accent.addEventListener('click', () => {
+    if (accent.classList.contains('off')) {
+        accent.classList.remove('off');
+        accent.style.backgroundColor = '#EC87A9';
+        accent.style.color = 'white';
+    } else {
+        accent.classList.add('off')
+        accent.style.backgroundColor = 'white';
+        accent.style.color = '#BC81BF';
+    }
+})
+
+let ghost = document.getElementById('ghost')
+ghost.addEventListener('click', () => {
+    if (accent.classList.contains('off')) {
+        accent.classList.remove('off');
+        accent.style.backgroundColor = '#EC87A9';
+        accent.style.color = 'white';
+    } else {
+        accent.classList.add('off')
+        accent.style.backgroundColor = 'white';
+        accent.style.color = '#BC81BF';
+    }
+})
+
+let undo = document.getElementById('undo')
+undo.addEventListener('click', () => {
+    if (accent.classList.contains('off')) {
+        accent.classList.remove('off');
+
+    } else {
+        accent.classList.add('off')
+
+    }
+})
+
+let copy = document.getElementById('copy')
+accent.addEventListener('click', () => {
+    if (accent.classList.contains('off')) {
+        accent.classList.remove('off');
+        accent.style.backgroundColor = '#EC87A9';
+        accent.style.color = 'white';
+    } else {
+        accent.classList.add('off')
+        accent.style.backgroundColor = 'white';
+        accent.style.color = '#BC81BF';
+    }
+})
+
+let length = document.getElementById('length')
+length.addEventListener('click', () => {
+    if (accent.classList.contains('off')) {
+        accent.classList.remove('off');
+        accent.style.backgroundColor = '#ECC987';
+        accent.style.color = 'white';
+    } else {
+        accent.classList.add('off')
+        accent.style.backgroundColor = 'white';
+        accent.style.color = '#BC81BF';
+    }
+})
