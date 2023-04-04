b5a9f661a1dff05085744180dcf94803753db846
muno17 <munozd17@gmail.com>
2023-04-03 23:13:22 -0400

    add functionality to buttonFunctions so that only one can be selected at a time but you can freely switch between any of them

--- static/javascript/noteFunctions.js
+++ static/javascript/noteFunctions.js
@@ -1,33 +1,46 @@
 export let accent = document.getElementById('accent')
 accent.addEventListener('click', () => {
-    if (ghost.classList.contains('off') && copy.classList.contains('off') 
-        && lengthButton.classList.contains('off')) {
-        if (accent.classList.contains('off')) {
-            accent.classList.remove('off');
-            accent.style.backgroundColor = '#F28D5F';
-            accent.style.color = 'white';
+    if (accent.classList.contains('off')) {
+        accent.classList.remove('off');
+        accent.style.backgroundColor = '#F28D5F';
+        accent.style.color = 'white';
 
-        } else {
-            accent.classList.add('off')
-            accent.style.backgroundColor = 'white';
-            accent.style.color = '#BC81BF';
-        }
+        lengthButton.classList.add('off')
+        lengthButton.style.backgroundColor = 'white';
+        lengthButton.style.color = '#BC81BF';
+        copy.classList.add('off')
+        copy.style.backgroundColor = 'white';
+        copy.style.color = '#BC81BF';
+        ghost.classList.add('off')
+        ghost.style.backgroundColor = 'white';
+        ghost.style.color = '#BC81BF';
+    } else {
+        accent.classList.add('off')
+        accent.style.backgroundColor = 'white';
+        accent.style.color = '#BC81BF';
     }
 })
 
 export let ghost = document.getElementById('ghost')
 ghost.addEventListener('click', () => {
-    if (accent.classList.contains('off') && copy.classList.contains('off') 
-        && lengthButton.classList.contains('off')) {
-        if (ghost.classList.contains('off')) {
-            ghost.classList.remove('off');
-            ghost.style.backgroundColor = '#8EDFB7';
-            ghost.style.color = 'white';
-        } else {
-            ghost.classList.add('off')
-            ghost.style.backgroundColor = 'white';
-            ghost.style.color = '#BC81BF';
-        }
+    if (ghost.classList.contains('off')) {
+        ghost.classList.remove('off');
+        ghost.style.backgroundColor = '#8EDFB7';
+        ghost.style.color = 'white';
+
+        lengthButton.classList.add('off')
+        lengthButton.style.backgroundColor = 'white';
+        lengthButton.style.color = '#BC81BF';
+        copy.classList.add('off')
+        copy.style.backgroundColor = 'white';
+        copy.style.color = '#BC81BF';
+        accent.classList.add('off')
+        accent.style.backgroundColor = 'white';
+        accent.style.color = '#BC81BF';
+    } else {
+        ghost.classList.add('off')
+        ghost.style.backgroundColor = 'white';
+        ghost.style.color = '#BC81BF';
     }
 })
 
@@ -64,6 +77,53 @@ export function ghoster(step) {
 
 
 
+let copy = document.getElementById('copy')
+copy.addEventListener('click', () => {
+    if (copy.classList.contains('off')) {
+        copy.classList.remove('off');
+        copy.style.backgroundColor = '#479682';
+        copy.style.color = 'white';
+
+        lengthButton.classList.add('off')
+        lengthButton.style.backgroundColor = 'white';
+        lengthButton.style.color = '#BC81BF';
+        ghost.classList.add('off')
+        ghost.style.backgroundColor = 'white';
+        ghost.style.color = '#BC81BF';
+        accent.classList.add('off')
+        accent.style.backgroundColor = 'white';
+        accent.style.color = '#BC81BF';
+    } else {
+        copy.classList.add('off')
+        copy.style.backgroundColor = 'white';
+        copy.style.color = '#BC81BF';
+    }
+})
+
+let lengthButton = document.getElementById('length')
+lengthButton.addEventListener('click', () => {
+    if (lengthButton.classList.contains('off')) {
+        lengthButton.classList.remove('off');
+        lengthButton.style.backgroundColor = '#ECE287';
+        lengthButton.style.color = 'white';
+
+        copy.classList.add('off')
+        copy.style.backgroundColor = 'white';
+        copy.style.color = '#BC81BF';
+        ghost.classList.add('off')
+        ghost.style.backgroundColor = 'white';
+        ghost.style.color = '#BC81BF';
+        accent.classList.add('off')
+        accent.style.backgroundColor = 'white';
+        accent.style.color = '#BC81BF';
+    } else {
+        lengthButton.classList.add('off')
+        lengthButton.style.backgroundColor = 'white';
+        lengthButton.style.color = '#BC81BF';
+    }
+})
+
+
 let undo = document.getElementById('undo')
 undo.addEventListener('click', () => {
     if (undo.classList.contains('off')) {
@@ -74,36 +134,3 @@ undo.addEventListener('click', () => {
 
     }
 })
-
-let copy = document.getElementById('copy')
-copy.addEventListener('click', () => {
-    if (ghost.classList.contains('off') && accent.classList.contains('off') 
-        && lengthButton.classList.contains('off')) {
-        if (copy.classList.contains('off')) {
-            copy.classList.remove('off');
-            copy.style.backgroundColor = '#479682';
-            copy.style.color = 'white';
-        } else {
-            copy.classList.add('off')
-            copy.style.backgroundColor = 'white';
-            copy.style.color = '#BC81BF';
-        }
-    }
-})
-
-let lengthButton = document.getElementById('length')
-lengthButton.addEventListener('click', () => {
-    if (ghost.classList.contains('off') && copy.classList.contains('off') 
-    && accent.classList.contains('off')) {
-        if (lengthButton.classList.contains('off')) {
-            lengthButton.classList.remove('off');
-            lengthButton.style.backgroundColor = '#ECE287';
-            lengthButton.style.color = 'white';
-        } else {
-            lengthButton.classList.add('off')
-            lengthButton.style.backgroundColor = 'white';
-            lengthButton.style.color = '#BC81BF';
-        }
-    }
-})
-
