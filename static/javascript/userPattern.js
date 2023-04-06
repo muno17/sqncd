2e03a4b6e475200623fe4882199219012fdcd1fa
muno17 <munozd17@gmail.com>
2023-04-06 19:12:23 -0400

    allow only one pitch button to be selected at a time

--- static/javascript/userPattern.js
+++ static/javascript/userPattern.js
@@ -2,20 +2,35 @@ import { keys } from '/static/javascript/notegen.js'
 
 let pitchButton = document.getElementsByClassName('pitchButton')
 
+// turn pitchButtons on and off when clicked
 for (let i = 0; i < 12; i++) {
     pitchButton[i].value = keys[i];
 
+
     pitchButton[i].addEventListener('click', () => {
-        if (pitchButton[i].classList.contains('off')) {
+        // get a reference to the currentPitch
+        let currentPitch = document.getElementsByClassName('currentPitch')
+        
+        // if no pitch button has been selected yet, make it the currentPitch
+        if (!currentPitch[0]) {
             pitchButton[i].classList.remove('off');
+            pitchButton[i].classList.add('currentPitch')
             pitchButton[i].style.backgroundColor = '#ECC987';
         } else {
-            pitchButton[i].classList.add('off');
-            if (pitchButton[i].classList.contains('flat')) {
-                pitchButton[i].style.backgroundColor = 'black';
+            // turn off currentPitch and and reset color
+            let oldPitch = currentPitch[0];
+            oldPitch.classList.remove('currentPitch');
+            oldPitch.classList.add('off');
+            if (oldPitch.classList.contains('flat')) {
+                oldPitch.style.backgroundColor = 'black';
             } else {
-                pitchButton[i].style.backgroundColor = 'white';
+                oldPitch.style.backgroundColor = 'white';
             }
+            
+            // make pitchButton the new currentPitch
+            pitchButton[i].classList.remove('off');
+            pitchButton[i].style.backgroundColor = '#ECC987';
+            pitchButton[i].classList.add('currentPitch')
         }
     })
 }
