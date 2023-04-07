12722756799b57cc2c378472a3cf5bde7e6542fa
muno17 <munozd17@gmail.com>
2023-04-07 15:43:51 -0400

    fully implement support to send clock to all devices

--- static/javascript/midi_io.js
+++ static/javascript/midi_io.js
@@ -46,34 +46,45 @@ export function sendMidi(n) {
         let outputDevice = WebMidi.getOutputByName(deviceValue);
         let outputChannel = outputDevice.channels[channel];
 
-        outputChannel.sendStart()
+
         outputChannel.playNote(note, {duration: 100, rawAttack: velocity})
         }
 }
 
-
 // send start and stop messages
 export function sendStartSignal() {
-    let outputDevice = WebMidi.getOutputByName(deviceValue);
-    outputDevice.sendStart()
-        // outputDevice.sendClock()
+    if (!clockButton.classList.contains('off')) {
+        for (let i = 0; i < deviceDropdown.length; i++) {
+            let x = WebMidi.getOutputByName(deviceDropdown[i].value);
+            x.sendStart()
+        }
+    } else {
+        let outputDevice = WebMidi.getOutputByName(deviceValue);
+        outputDevice.sendStart()
+    }
 }
 
 export function sendStopSignal() {
-    let outputDevice = WebMidi.getOutputByName(deviceValue);
-    outputDevice.sendStop()
+    if (!clockButton.classList.contains('off')) {
+        for (let i = 0; i < deviceDropdown.length; i++) {
+            let x = WebMidi.getOutputByName(deviceDropdown[i].value);
+            x.sendStop()
+        }
+    } else {
+        let outputDevice = WebMidi.getOutputByName(deviceValue);
+        outputDevice.sendStop()
+    }
 }
 
-export function sendClockSignal(device) {
-    let device = WebMidi.getOutputByName(device);
-    device.sendClock()
+export function sendClockSignal(clockDevice) {
+    // console.log(WebMidi.getOutputByName(deviceValue))
+    // console.log(clockDevice)
+    let sendToDevice = WebMidi.getOutputByName(clockDevice);
+    sendToDevice.sendClock()
 }
-
  
 export let clockButton = document.getElementById('clockSwitch')
 clockButton.addEventListener('click', () => {
-    // let outputDevice = WebMidi.getOutputByName(deviceValue);
-    // console.log(outputDevice)
     if (clockButton.classList.contains('off')) {
         clockButton.classList.remove('off')
     } else {
@@ -81,6 +92,15 @@ clockButton.addEventListener('click', () => {
     }
 })
 
-function clockDevices() {
-    
+export function clockDevices() {
+    let devicesLength = deviceDropdown.length
+
+    // create an array to store devices to receive clock
+    let sendClockTo = []
+
+    // loop through and add all devices that are not currently selected to sendClockTo[]
+    for (let i = 0; i < devicesLength; i++) {
+            sendClockTo.push(deviceDropdown[i].value);
+    }
+    return sendClockTo;
 }
